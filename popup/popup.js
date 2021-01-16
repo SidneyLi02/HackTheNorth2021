6d1b34826fd6b42f5364cb2bd8450a2f736e87de
SidneyLi02 <66651189+SidneyLi02@users.noreply.github.com>
2021-01-16 12:24:09 -0800

    update

--- popup/popup.js
+++ popup/popup.js
@@ -1,13 +1,19 @@
-const popupRun = () => {
+const run = () => {
 
-    const popupBody = document.addEventListener('.extension-body');
-    const findOppose = document.querySelector('#find-oppose');
-    const findSimilar = document.querySelector('#find-similar');
+    const popupBody = document.querySelector('.init');
+    const findOppose = document.querySelectorAll('.extension-body button')[0];
+    const findSimilar = document.querySelectorAll('.extension-body button')[1];
+    const searchResultsBody = document.querySelectorAll('.displayNews');
 
-    findOppose.onClick = () => {
-        popupBody.classList.add("fadeOut");
-    }
-};
+    findOppose.addEventListener("click", () => {
+        popupBody.classList.add("fadeOut")
+        searchResultsBody.classList.add("fadeIn")
+    })
+    
+    findSimilar.addEventListener("click", () => {
+        popupBody.classList.add("fadeOut")
+        searchResultsBody.classList.add("fadeIn")
+    })
 
-popupRun();
-
+}
+run();
