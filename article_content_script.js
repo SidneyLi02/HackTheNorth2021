b9c8cd7af6f9d90ea45f198b68d9515f71de2eb7
Amrit Sehgal <AmritGithub@gmail.com>
2021-01-17 02:12:56 -0600

    Fixed fetching

--- article_content_script.js
+++ article_content_script.js
@@ -1,35 +1,46 @@
-var xmlHttp = new XMLHttpRequest();;
+/*
+var xmlHttp = new XMLHttpRequest();
 var arrOpposing;
 var arrAgreeing;
 var domainName;
 var title;
 let agreeArr = [];
 let oppArr = [];
+var obtainedResults = false;
 
-function httpGetAsync(theUrl, callback)
+function httpGetAsync(theUrl)
 {
       xmlHttp.onreadystatechange = function() { 
           if (xmlHttp.readyState == 4 && xmlHttp.status == 200) {
-              callback(xmlHttp.responseText);
+            results = JSON.parse(xmlHttp.responseText);
+            console.log("Api has been called");
+            callBackTest();
       }
       xmlHttp.open("GET", theUrl, true);
       xmlHttp.send(null);
     }
 }
+*/
 
-let reqUrl = 'https://www.googleapis.com/customsearch/v1?cx=b84518462114f3218&excludeTerms='+(domainName)+'&lr=%22lang_en%22&q='+(title)+
-'&key=AIzaSyDAFluHuEtmYiWUpN6uKWgIQSuWXLmtDsA&cxb84518462114f3218';
-const testingFunc = (a) => console.log(a);
-httpGetAsync(reqUrl, testingFunc);
+
+//var reqUrl = 'https://www.googleapis.com/customsearch/v1?cx=b84518462114f3218&excludeTerms='+(domainName)+'&lr=%22lang_en%22&q='+(title)+
+//'&key=AIzaSyDAFluHuEtmYiWUpN6uKWgIQSuWXLmtDsA&cxb84518462114f3218';
+//const testingFunc = (a) => console.log(a);
+// httpGetAsync(reqUrl);
+//results = JSON.parse(xmlHttp.responseText);
+//console.log("Api has been called");
+//console.log(results);
+//function callBackTest() {
+    /*
 results = JSON.parse(xmlHttp.responseText);
 console.log("Api has been called");
 console.log(results);
-
-function isOpposingView(index)
+*/
+function isOpposingView(index, arrAgreeing2, results)
 {
     let acc = true;
-    for (let i = 0; i<arrAgreeing.length; i++) {
-        if (results.items[index].displayLink.includes(arrAgreeing[i])) {
+    for (let i = 0; i<arrAgreeing2.length; i++) {
+        if (results.items[index].displayLink.includes(arrAgreeing2[i])) {
             acc = acc && false;
         } else {
             acc = acc && true;
@@ -38,11 +49,11 @@ function isOpposingView(index)
     return acc;
 }
 
-function isSameView(index)
+function isSameView(index, arrOpposing2, results)
 {
     let acc = true;
-    for (let i = 0; i<arrOpposing.length; i++) {
-        if (results.items[index].displayLink.includes(arrOpposing[i])) {
+    for (let i = 0; i<arrOpposing2.length; i++) {
+        if (results.items[index].displayLink.includes(arrOpposing2[i])) {
             acc = acc && true;
         } else {
             acc = acc && false;
@@ -51,7 +62,7 @@ function isSameView(index)
     return acc;
 }
 
-
+/*
 for (let i = 1; i<=50; i++) {
     if (isOpposingView(i)) { // checking if the webpage is opposing view
         oppArr.push({link: results.items[i].link, title: results.items[i].title});
@@ -62,6 +73,18 @@ for (let i = 1; i<=50; i++) {
         agreeArr.push({link: results.items[i].link, title: results.items[i].title});
     }
 }
+obtainedResults = true;
+}
+*/
+
+var port = chrome.runtime.connect({name: "otherArticles"});
+port.onMessage.addListener(function(msg) {
+    if (msg.type == "displayOtherArticles") {
+        if (obtainedResults == true) {
+            port.postMessage({type: "sentResults", agree: agreeArr, oppose: oppArr});
+        }
+    }
+});
 
 chrome.runtime.onMessage.addListener(
     function(request, sender, sendResponse) {
@@ -76,8 +99,33 @@ chrome.runtime.onMessage.addListener(
             }
             title = request.title;
             domainName = request.domain;
-          case "displayOtherArticles":
-              sendResponse({agree: agreeArr, oppose: oppArr});
+            console.log("doing fetch");
+            fetch('https://www.googleapis.com/customsearch/v1?cx=b84518462114f3218&excludeTerms='+(domainName)+'&lr=%22lang_en%22&q='+(title)+
+            '&key=AIzaSyDAFluHuEtmYiWUpN6uKWgIQSuWXLmtDsA&cxb84518462114f3218')
+  .then(response => response.json())
+  .then(data2 => {
+
+    let data = JSON.parse(data2);
+    
+    
+    for (let i = 1; i<=50; i++) {
+        if (isOpposingView(i, arrAgreeing, data)) { // checking if the webpage is opposing view
+            oppArr.push({link: data.items[i].link, title: data.items[i].title});
+        }
+    }
+    for (let i = 1; i<=50; i++) {
+        if (isSameView(i, arrOpposing, data)) { // checking if the webpage is same view
+            agreeArr.push({link: data.items[i].link, title: data.items[i].title});
+        }
+    }
+
+
+
+
+
+
+  })
+  .catch(err => console.log(err));
           default:
               console.log("Unrecognized message: ", request);
       }
